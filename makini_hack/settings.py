8e0edfa617bda9f4950263927c6c4db2e56c4fc7
Brian Kamotho <kariukibriankamotho@gmail.com>
2024-03-23 08:52:46 +0300

    wowooooo

--- makini_hack/settings.py
+++ makini_hack/settings.py
@@ -117,3 +117,11 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'kariukibriankamotho@gmail.com'
+EMAIL_HOST_PASSWORD = 'fbgu hcfw vauj odos'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
--- web/views.py
+++ web/views.py
@@ -1,6 +1,31 @@
+from django.core.mail import send_mail
+from makini_hack.settings import EMAIL_HOST_USER
 from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+
+def contact(request):
+    if request.method == 'POST':
+        message_name = request.POST['message-name']
+        message_email = request.POSTp['mesaage-email']
+        message =request.POST['message']
+
+        # send an email
+        send_mail(
+            message_name, #subjetc,
+            message,
+            message_email ,
+            ['kariukibriankamotho@gmail.com'],
+        )
+    else:
+        return render(request, 'contact.html', {})
+
+    return render(request, 'contact.html', {})
